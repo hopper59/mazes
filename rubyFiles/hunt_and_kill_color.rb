62593eec7961ddfc58c971ccb21ea40a10560058
hopper59 <tenchii11@hotmail.com>
2015-03-22 15:17:15 -0400

    added hunt and kill algo and its color demo

---
+++ rubyFiles/hunt_and_kill_color.rb
@@ -0,0 +1,15 @@
+require 'colored_grid'
+require 'hunt_and_kill'
+
+grid = ColoredGrid.new(20,20)
+HuntAndKill.on(grid)
+
+middle = grid[grid.rows / 2, grid.columns / 2]
+
+grid.distances = middle.distances
+
+filename = 'hunt_and_kill_color.png'
+grid.to_png.save(filename)
+
+puts "saved to #{filename}"
+
