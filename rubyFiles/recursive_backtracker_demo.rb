f0e36cd584d7f47e14be8ea5f91712973c49c8b5
hopper59 <tenchii11@hotmail.com>
2015-03-22 22:22:23 -0400

    inset part 1

--- rubyFiles/recursive_backtracker_demo.rb
+++ rubyFiles/recursive_backtracker_demo.rb
@@ -6,5 +6,6 @@ RecursiveBacktracker.on(grid)
 grid.braid(0.5)
 
 filename = 'recursive_backtracker.png'
-grid.to_png.save(filename)
+#grid.to_png.save(filename)
+grid.to_png(10,0.1).save(filename)
 puts "saved to #{filename}"
