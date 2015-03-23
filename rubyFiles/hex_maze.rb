9a79330226ac60e2cb6af824c70fcae4f991abe3
hopper59 <tenchii11@hotmail.com>
2015-03-22 20:48:17 -0400

    added hex mazes

---
+++ rubyFiles/hex_maze.rb
@@ -0,0 +1,10 @@
+require 'hex_grid'
+require 'recursive_backtracker'
+
+grid = HexGrid.new(10,10)
+RecursiveBacktracker.on(grid)
+
+filename = 'hex.png'
+grid.to_png.save(filename)
+puts "saved to #{filename}"
+
